869d3ac84038635a7bd79f6d9ac58bc5c0254ef5
azamf <azamfathur32@gmail.com>
2025-10-19 11:41:20 +0700

    Initial Commit

---
+++ src/app/components/Cam.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cam() {
+  return (
+    <div className='h-[450] border-2'>Cam</div>
+  )
+}
+
+export default Cam
---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <p className='text-center text-xs'>&copy; 2025 CuyFace. Ramalan ngasbun yang katanya pakai AI</p>
+    </footer>
+  )
+}
+
+export default Footer
